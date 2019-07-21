bf0551d04c179c2528f4e29536048c0393a67eff
Hugo Gu <hugogu@outlook.com>
2019-07-21 19:51:27 +0800

    Integrate with crnk.io to expose JSON API web interfaces.

--- postgres/src/main/kotlin/hugo/io/spring/postgres/domain/Person.kt
+++ postgres/src/main/kotlin/hugo/io/spring/postgres/domain/Person.kt
@@ -1,10 +1,12 @@
 package hugo.io.spring.postgres.domain
 
+import io.crnk.core.resource.annotations.JsonApiResource
 import org.hibernate.annotations.Type
 import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity
+@JsonApiResource(type = "person")
 data class Person(val idNumber: String) : AbstractEntity() {
     var name: String = ""
 
