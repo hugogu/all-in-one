5bd4a85743e3c942ce3d06470698309354ab90a8
Hugo Gu <hugogu@outlook.com>
2019-08-09 00:05:04 +0800

    Enable Swagger.

--- postgres/src/main/kotlin/hugo/io/spring/postgres/PostgresApplication.kt
+++ postgres/src/main/kotlin/hugo/io/spring/postgres/PostgresApplication.kt
@@ -3,9 +3,11 @@ package hugo.io.spring.postgres
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.SpringApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableSwagger2
 open class PostgresApplication
 
 fun main(args: Array<String>) {
